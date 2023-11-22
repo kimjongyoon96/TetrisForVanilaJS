9c360548666402217f600d961e3ef2d063039013
kimjongyoon96 <kimjongyoon96@gmail.com>
2023-11-22 14:22:14 +0900

    테트리스 만들기 시작

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gameBoard = document.getElementById("gameBoard");
+  const scoreDisplay = document.getElementById("score");
+  const startButton = document.getElementById("startButton");
+
+  // 테트리스 로직 구현
+  // 예: 게임 보드 초기화, 테트리미노 생성 및 이동, 점수 계산 등
+
+  startButton.addEventListener("click", () => {
+    // 게임 시작 로직
+  });
+});
