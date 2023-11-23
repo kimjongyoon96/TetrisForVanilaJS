68421810cf14c1f51fd7a318a0ff65b4c9a2976a
kimjongyoon96 <kimjongyoon96@gmail.com>
2023-11-23 11:55:33 +0900

    현재 상태정보 객체 생성 및 배열에 저장되게 끔 저장

---
+++ cute.js
@@ -0,0 +1,3 @@
+document.addEventListener("keydown", function (event) {
+  console.log("Pressed key:", event.key);
+});
--- index.js
+++ index.js
@@ -9,7 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log(cells);
 
   /* cell에 대한 참조를 위해 배열을 선언해주었다*/
-  const gameBoardCells = [];
+  const gameBoardCells = []; // 게임보드판 셀들을 배열에 넣는다.
+  console.log(gameBoardCells);
   for (let i = 0; i < 200; i++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
@@ -17,20 +18,27 @@ document.addEventListener("DOMContentLoaded", () => {
     gameBoardCells.push(cell); // 배열에 셀 참조 추가
   }
 
-  startButton.addEventListener("click", () => {
-    const randomTetrimino = tetriminoGenerator.getRandomTetrimino();
-    console.log(randomTetrimino);
+  // 사용자 정보처리를 위한 객체의 필요성
+  let currentTetrimino = {
+    shape: [],
+    position: [],
+    type: [],
+  };
 
-    const initialPosition = { x: 4, y: 0 }; // 예시 초기 위치
-    console.log(initialPosition);
+  console.log(currentTetrimino);
+  console.log("여기 제대로 오는가?");
+  startButton.addEventListener("click", () => {
     const randomTetriminoData = tetriminoGenerator.getRandomTetrimino();
+    currentTetrimino.shape = randomTetriminoData.shape;
+    currentTetrimino.position = { x: 4, y: 0 };
+    currentTetrimino.type = randomTetriminoData.type;
     drawTetrimino(
       gameBoardCells,
-      randomTetriminoData.shape,
-      initialPosition,
-      randomTetriminoData.type
+      currentTetrimino.shape,
+      currentTetrimino.position,
+      currentTetrimino.type
     );
+    console.log(currentTetrimino.type);
+    console.log(currentTetrimino);
   });
-
-  console.log(drawTetrimino);
 });
