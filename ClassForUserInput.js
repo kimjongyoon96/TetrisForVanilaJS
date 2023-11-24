a8d0fea1bdea991540d548e22915f68c2b74ede2
kimjongyoon96 <kimjongyoon96@gmail.com>
2023-11-24 18:57:53 +0900

    class 사용하여 모듈화 완료, 사용자 입력

---
+++ ClassForUserInput.js
@@ -0,0 +1,67 @@
+// UserInputHandler.js
+import { currentTetrimino } from "../TetriminoCurrentPosition.js";
+import { updateGameBoard } from "../UpdateGameBoard.js";
+import TetriminoGenerator from "../random.js";
+
+class UserInputHandler {
+  constructor(gameBoardCells) {
+    this.gameBoardCells = gameBoardCells;
+    this.tetriminoGenerator = new TetriminoGenerator();
+    this.addKeyboardListeners();
+  }
+
+  addKeyboardListeners() {
+    document.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "ArrowLeft":
+          this.moveLeft();
+          break;
+        case "ArrowRight":
+          this.moveRight();
+          break;
+        case "ArrowDown":
+          this.moveDown();
+          break;
+        case "ArrowUp":
+          this.rotate();
+          break;
+      }
+    });
+  }
+
+  moveDown() {
+    if (currentTetrimino.position.y < 19) {
+      currentTetrimino.position.y += 1;
+      updateGameBoard(this.gameBoardCells);
+    }
+  }
+
+  moveLeft() {
+    if (currentTetrimino.position.x > 0) {
+      currentTetrimino.position.x -= 1;
+      updateGameBoard(this.updateGameBoard);
+    }
+  }
+
+  moveRight() {
+    if (currentTetrimino.position.x < 9) {
+      currentTetrimino.position.x += 1;
+      updateGameBoard(this.updateGameBoard);
+    }
+  }
+
+  rotate() {
+    currentTetrimino.rotation = (currentTetrimino.rotation + 90) % 360;
+    console.log(currentTetrimino.rotation); // 회전방향 즉 90도,180도,270도..
+    let rotatedType = currentTetrimino.type + currentTetrimino.rotation;
+    if (currentTetrimino.rotation === 0) {
+      currentTetrimino.rotation = 0;
+      rotatedType = currentTetrimino.type;
+      console.log("cultureshock");
+    }
+    currentTetrimino.shape = this.tetriminoGenerator.tetriminos[rotatedType];
+    updateGameBoard(this.updateGameBoard);
+  }
+}
+
+export default UserInputHandler;
--- Common/index.js
+++ Common/index.js
@@ -2,6 +2,7 @@ import TetriminoGenerator from "../random.js";
 import { drawTetrimino } from "../drawTetrimino.js";
 import { currentTetrimino } from "../TetriminoCurrentPosition.js";
 import { setupGameBoard } from "../GameBoard.js";
+import UserInputHandler from "../ClassForUserInput.js";
 import { updateGameBoard } from "../UpdateGameBoard.js";
 
 export const gameBoardCells = setupGameBoard(gameBoard); // 게임보드 모듈 from GameBoard.js
@@ -10,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const gameBoard = document.getElementById("gameBoard");
   const startButton = document.getElementById("startButton");
   const tetriminoGenerator = new TetriminoGenerator(); // 클래스 인스턴스 from random.js
+  const userInputHandler = new UserInputHandler(gameBoardCells);
 
   startButton.addEventListener("click", () => {
     const randomTetriminoData = tetriminoGenerator.getRandomTetrimino();
@@ -25,64 +27,65 @@ document.addEventListener("DOMContentLoaded", () => {
     );
     console.log(currentTetrimino.type); // s
   });
-  //   모듈1-movingDown.js
-  function movieDown() {
-    if (currentTetrimino.position.y < 19) {
-      currentTetrimino.position.y += 1;
-      updateGameBoard();
-    }
-    console.log(currentTetrimino.position.y);
-  }
-  // 모듈2-movingLeft.js
-  function movieLeft() {
-    if (currentTetrimino.position.x > 0) {
-      currentTetrimino.position.x -= 1;
-      updateGameBoard();
-    }
-  }
-  // 모듈3-movingRight.js
-  function movieRight() {
-    if (currentTetrimino.position.x < 9) {
-      currentTetrimino.position.x += 1;
-      updateGameBoard();
-    }
-  }
-  // 모듈4-movingRotation.js
-  function movieRotate() {
-    currentTetrimino.rotation = (currentTetrimino.rotation + 90) % 360;
-    console.log(currentTetrimino.rotation); // 회전방향 즉 90도,180도,270도..
-    let rotatedType = currentTetrimino.type + currentTetrimino.rotation;
-    if (currentTetrimino.rotation === 0) {
-      currentTetrimino.rotation = 0;
-      rotatedType = currentTetrimino.type;
-      console.log("cut");
-    }
 
-    console.log(rotatedType); // z90, z180, z270, z0
+  // //   모듈1-movingDown.js
+  // function movieDown() {
+  //   if (currentTetrimino.position.y < 19) {
+  //     currentTetrimino.position.y += 1;
+  //     updateGameBoard();
+  //   }
+  //   console.log(currentTetrimino.position.y);
+  // }
+  // // 모듈2-movingLeft.js
+  // function movieLeft() {
+  //   if (currentTetrimino.position.x > 0) {
+  //     currentTetrimino.position.x -= 1;
+  //     updateGameBoard();
+  //   }
+  // }
+  // // 모듈3-movingRight.js
+  // function movieRight() {
+  //   if (currentTetrimino.position.x < 9) {
+  //     currentTetrimino.position.x += 1;
+  //     updateGameBoard();
+  //   }
+  // }
+  // // 모듈4-movingRotation.js
+  // function movieRotate() {
+  //   currentTetrimino.rotation = (currentTetrimino.rotation + 90) % 360;
+  //   console.log(currentTetrimino.rotation); // 회전방향 즉 90도,180도,270도..
+  //   let rotatedType = currentTetrimino.type + currentTetrimino.rotation;
+  //   if (currentTetrimino.rotation === 0) {
+  //     currentTetrimino.rotation = 0;
+  //     rotatedType = currentTetrimino.type;
+  //     console.log("cut");
+  //   }
 
-    currentTetrimino.shape = tetriminoGenerator.tetriminos[rotatedType];
-    updateGameBoard();
-  }
+  //   console.log(rotatedType); // z90, z180, z270, z0
 
-  // current 정보를 바탕으로 테트리미노 조작 로직
-  document.addEventListener("keydown", (e) => {
-    switch (e.key) {
-      case "ArrowLeft":
-        movieLeft();
-        console.log("왼쪽 눌렀다.");
-        break;
-      case "ArrowRight":
-        movieRight();
-        console.log("오른쪽 눌렀다.");
-        break;
-      case "ArrowDown":
-        movieDown();
-        console.log("아래버튼 눌렀다.");
-        break;
-      case "ArrowUp":
-        movieRotate();
-        console.log("회전버튼 눌렀다.");
-        break;
-    }
-  });
+  //   currentTetrimino.shape = tetriminoGenerator.tetriminos[rotatedType];
+  //   updateGameBoard();
+  // }
+
+  // // current 정보를 바탕으로 테트리미노 조작 로직
+  // document.addEventListener("keydown", (e) => {
+  //   switch (e.key) {
+  //     case "ArrowLeft":
+  //       movieLeft();
+  //       console.log("왼쪽 눌렀다.");
+  //       break;
+  //     case "ArrowRight":
+  //       movieRight();
+  //       console.log("오른쪽 눌렀다.");
+  //       break;
+  //     case "ArrowDown":
+  //       movieDown();
+  //       console.log("아래버튼 눌렀다.");
+  //       break;
+  //     case "ArrowUp":
+  //       movieRotate();
+  //       console.log("회전버튼 눌렀다.");
+  //       break;
+  //   }
+  // });
 });
