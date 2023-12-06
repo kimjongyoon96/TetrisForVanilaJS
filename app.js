f0883fab8dce9ea88e754a6585ed1e68355db48a
kimjongyoon96 <kimjongyoon96@gmail.com>
2023-12-06 16:58:57 +0900

    완성

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const htmlbox = require("./htmlbox");
+app.use(express.static("public"));
+
+app.get("/tetris", (req, res) => {
+  res.send(htmlbox.htmlFunc(htmlbox.mySource));
+});
+
+// 서버를 시작합니다.
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port}에서 실행 중입니다.`);
+});
+
+// if (request.url.startsWith("/tetris")) {
+//   response.writeHead(200, { "Content-Type": "text/html" });
+//   response.end(html.htmlFunc(htmlBox.mySource));
+// }
---
+++ public/Clock.js
@@ -0,0 +1,19 @@
+// import { gameOverModal } from "./ModalForGameOver";
+export function startCountdown(duration) {
+  let remainingTime = duration;
+  const clockElement = document.getElementById("clock");
+
+  const timer = setInterval(() => {
+    clockElement.textContent = remainingTime + "초 ";
+
+    remainingTime -= 1;
+
+    if (remainingTime < 0) {
+      clearInterval(timer);
+      clockElement.textContent = "0";
+      // gameOverModal();
+    }
+  }, 1000);
+}
+
+startCountdown(60); // 60초에서 시작하는 카운트다운
